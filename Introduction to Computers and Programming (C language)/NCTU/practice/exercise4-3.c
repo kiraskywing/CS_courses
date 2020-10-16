c1722de2c7853acbcea7633f137424eb914bf104
Yi-Chang Lin <kiraskywing@gmail.com>
2020-10-16 10:57:56 +0800

    Add practice Learned from NCTU 20201016

---
+++ Introduction to Computers and Programming (C language)/NCTU/practice/exercise4-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("8 / 5 = %d\n", 8 / 5);
+    printf("-8 / 5 = %d\n", -8 / 5);
+    printf("-(8 / 5) = %d\n", -(8 / 5));
+    printf("8 / -5 = %d\n", 8 / -5);
+    printf("-8 / -5 = %d\n", -8 / -5);
+    return 0;
+}
---
+++ Introduction to Computers and Programming (C language)/NCTU/practice/exercise5-2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("!2 = %d\n", !2);
+    printf("!!2 = %d\n", !!2);
+    return 0;
+}
