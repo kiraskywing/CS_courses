f609051c1ef7c83505467ed5d5191fd01f29677f
Yi-Chang Lin <kiraskywing@gmail.com>
2020-10-19 16:42:01 +0800

    modify practice 20201019

--- Introduction to Computers and Programming (C language)/NCTU/practice/printf_examples.c
+++ Introduction to Computers and Programming (C language)/NCTU/practice/printf_examples.c
@@ -33,6 +33,7 @@ int main(void)
     float x = 839.21;
     printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
     printf("|%10.3f|%10.3e|%-10g|\n", x, x, x);
+    printf("%6d,%6d", 86, 1040);
 
     return 0;
 }
