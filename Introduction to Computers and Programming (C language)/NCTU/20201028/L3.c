92868188aa8213db53212cfe3efc5a90599f2b8a
Yi-Chang Lin <kiraskywing@gmail.com>
2020-10-28 19:37:48 +0800

    Modify one practice 20201028

--- Introduction to Computers and Programming (C language)/NCTU/20201028/L3.c
+++ Introduction to Computers and Programming (C language)/NCTU/20201028/L3.c
@@ -11,18 +11,14 @@ int main()
             arr[i][j] = 1;
     }
 
-    int times, x, y, x_min, x_max, y_min, y_max;
+    int times, x, y;
     scanf("%d\n", &times);
     for (int t = 0; t < times; t++)
     {
         scanf("%d %d\n", &x, &y);
-        x_min = x - 1 > 0 ? x - 1 : 0;
-        x_max = x + 1 < m ? x + 1 : m - 1;
-        y_min = y - 1 > 0 ? y - 1 : 0;
-        y_max = y + 1 < n ? y + 1 : n - 1;
-        for (int i = x_min; i <= x_max; i++)
+        for (int i = (x - 1 > 0 ? x - 1 : 0); i <= (x + 1 < m ? x + 1 : m - 1); i++)
         {
-            for (int j = y_min; j <= y_max; j++)
+            for (int j = (y - 1 > 0 ? y - 1 : 0); j <= (y + 1 < n ? y + 1 : n - 1); j++)
                 arr[i][j] = 0;
         }
     }
