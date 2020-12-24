7c847aac6d2e354b7dd8bdd842c8af5be095d2fa
Yi-Chang Lin <kiraskywing@gmail.com>
2020-12-24 15:54:57 +0800

    Update NCTU C Lang practice 20201224

--- Introduction to Computers and Programming (C language)/NCTU/20201221/no1257.c
+++ Introduction to Computers and Programming (C language)/NCTU/20201221/no1257.c
@@ -1,10 +1,10 @@
 /*
-Feature: 使用doubly linked list
+Feature: 用struct記錄個人資訊並與他人串接, 最後用DFS更新長幼順序
 
 step1. 開一個pointer array記錄每個人的資訊, 按照input順序排列; 把Dong放在index = 0
 step2. 用list_check檢查人名是否已記錄在pointer array, 找到就回傳該人對應pointer; 沒有就建立新node, 將其pointer記錄在array並回傳
-step3. 將parent和child按照關係連接, 若和Dong有搭上關係, 更新rank
-step4. traverse所有人, 更新每個人的rank
+step3. 將parent和child按照關係連接
+step4. 從Dong為起點進行DFS, traverse所有和Dong相接的人並更新related, rank
 step5. 按照pointer array輸出結果
 */
 
@@ -15,20 +15,21 @@ step5. 按照pointer array輸出結果
 
 struct node
 {
-    char name[30];
+    char name[30];                // 人名長度小於30
     int rank;
-    bool checked;    // 是否與Dong搭上關係
-    struct node *parent;
-    struct node *child;
+    bool related;
+    int n_elder, n_younger;       // 記錄長輩數, 子孫數
+    struct node *elders[30];      // 0 < N < 30
+    struct node *youngers[30];    // 因此一個人的長輩或子孫數量不會超過30人
 };
 
-struct node *ptr[30];    // for step1.
+struct node *ptr[30];    // for step1, input人數不超過30人
 int idx = 1;             // Dong放在index = 0, 因此從index = 1開始排
 
 void Dong_init(void);    // create Dong's node
-struct node* list_check(char *s);    // for step2.
-void connect(struct node *c, struct node *p);    // for step3.
-void update_rank(struct node *cur);    // for step4.
+struct node* list_check(char *s);    // for step2
+void connect(struct node *c, struct node *p);    // for step3
+void update_rank(struct node *cur, int rank);    // for step4
 void judge(struct node *cur);    // for step5
 
 int main(void)
@@ -37,7 +38,7 @@ int main(void)
     
     int n;
     scanf("%d", &n);
-    char s1[30] = "", s2[30] = "";
+    char s1[30] = "", s2[30] = "";           // 人名長度小於30
     for (int i = 0; i < n; i++)
     {
         scanf("%s %s", s1, s2);
@@ -46,8 +47,9 @@ int main(void)
         connect(n1, n2);                     // step3
     }
 
-    for (int i = 1; i < idx; i++)
-        update_rank(ptr[i]);                 // step4
+    update_rank(ptr[0], 0);                  
+    // step4, 以Dong為rank = 0, elder's rank > 0, younger's rank < 0
+    
     for (int i = 1; i < idx; i++)
         judge(ptr[i]);                       // step5
     
@@ -60,10 +62,9 @@ void Dong_init(void)
 {
     ptr[0] = (struct node*) malloc(sizeof(struct node));
     strcpy(ptr[0]->name, "Dong");
-    ptr[0]->rank = 0;          // 以Dong為0, elder's rank > 0, younger's rank < 0
-    ptr[0]->checked = true;    // 自己和自己有關係
-    ptr[0]->parent = NULL;
-    ptr[0]->child = NULL;
+    ptr[0]->related = false;
+    ptr[0]->n_elder = 0;
+    ptr[0]->n_younger = 0;
 }
 
 struct node* list_check(char *s)
@@ -80,9 +81,9 @@ struct node* list_check(char *s)
     // 不在array, 建一個新node
     struct node *new = (struct node*) malloc(sizeof(struct node));
     strcpy(new->name, s);
-    new->checked = false;    // 還未與Dong確認關係, initialized as false
-    new->parent = NULL;
-    new->child = NULL;
+    new->related = false;    // 還未與Dong確認關係, initialized as false
+    new->n_elder = 0;
+    new->n_younger = 0;
     
     ptr[idx] = new;          // 放入pointer array
     idx++;                   // 更新unique name數量
@@ -91,48 +92,29 @@ struct node* list_check(char *s)
 
 void connect(struct node *c, struct node *p)
 {
-    // parent, child互接
-    c->parent = p;
-    p->child = c;
-
-    if (c->checked)    // child和Dong有搭上關係
-    {
-        p->checked = true;
-        p->rank = c->rank + 1;    // parent's rank is higher
-    }
-    else if (p->checked)    // parent和Dong有搭上關係
-    {
-        c->checked = true;
-        c->rank = p->rank - 1;    // child's rank is lower
-    }
+    // parent, child互接, 並更新各自elder和younger數量
+    c->elders[c->n_elder++] = p;
+    p->youngers[p->n_younger++] = c;
 }
 
-void update_rank(struct node *cur)
+void update_rank(struct node *cur, int rank)
 {
-    struct node *temp;
+    if (cur->related)    // 遞迴出口, 造訪過就離開
+        return;
     
-    // 自己和Dong有搭上關係, 讓子孫都和Dong搭上關係
-    temp = cur;
-    while (temp->checked && temp->child && !temp->child->checked)
-    {
-        temp->child->checked = true;
-        temp->child->rank = temp->rank - 1;
-        temp = temp->child;
-    }
+    cur->related = true;
+    cur->rank = rank;
     
-    // 自己和Dong有搭上關係, 讓長輩都和Dong搭上關係
-    temp = cur;
-    while (temp->checked && temp->parent && !temp->parent->checked)
-    {
-        temp->parent->checked = true;
-        temp->parent->rank = temp->rank + 1;
-        temp = temp->parent;
-    }
+    for (int i = 0; i < cur->n_elder; i++)    // DFS, 往長輩方向, 長輩rank = 目前rank + 1
+        update_rank(cur->elders[i], rank + 1);
+
+    for (int i = 0; i < cur->n_younger; i++)    // DFS, 往子孫方向, 子孫rank = 目前rank - 1
+        update_rank(cur->youngers[i], rank - 1);
 }
 
 void judge(struct node *cur)
 {
-    if (!cur->checked)    // 沒關係
+    if (!cur->related)    // 沒關係
         printf("%s %s\n", cur->name, "unknown");
     else                  // 有關係, 照rank大小處理輸出
     {
--- Introduction to Computers and Programming (C language)/NCTU/20201221/no1258.c
+++ Introduction to Computers and Programming (C language)/NCTU/20201221/no1258.c
@@ -1,7 +1,12 @@
+/*
+建立一個pointer array儲存每個item的address
+使用qsort的時候, 是交換item的address, 不用搬動struct內的element
+理論上會比較有效率?
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 struct item
 {
@@ -9,10 +14,11 @@ struct item
     char name[11];
 };
 
-void init_list(struct item list[], int n);
-int buy(struct item list[], int n, int budget, char res[][11], int *count);
+void init_list(struct item *list[], int n);
+int buy(struct item *list[], int n, int budget, char res[][11], int *count);
 int rank_cmp(const void *a, const void *b);
 int name_cmp(const void *a, const void *b);
+void clear_ptrs(struct item *list[], int n);
 
 int main(void)
 {
@@ -23,9 +29,9 @@ int main(void)
     {
         scanf("%d %d", &budget, &n);
         
-        struct item list[n];
+        struct item *list[n];
         init_list(list, n);
-        qsort(list, n, sizeof(struct item), rank_cmp);
+        qsort(list, n, sizeof(struct item*), rank_cmp);
         
         char res[n][11];
         int count = 0;
@@ -36,42 +42,54 @@ int main(void)
         for (int i = 0; i < count; i++)
             printf("%s ", res[i]);
         puts("");
+
+        clear_ptrs(list, n);
     }
 
     return 0;
 }
 
-void init_list(struct item list[], int n)
+void init_list(struct item *list[], int n)
 {
     for (int i = 0; i < n; i++)
-        scanf("%d %d %s", &list[i].rank, &list[i].price, list[i].name);
+    {
+        struct item *new = (struct item*) malloc(sizeof(struct item));
+        scanf("%d %d %s", &new->rank, &new->price, new->name);
+        list[i] = new;
+    }
 }
 
-int buy(struct item list[], int n, int budget, char res[][11], int *count)
+int buy(struct item *list[], int n, int budget, char res[][11], int *count)
 {
     int remain = budget;
     for (int i = 0; i < n; i++)
     {
-        if (remain >= list[i].price)
+        if (remain >= list[i]->price)
         {
-            strcpy(res[*count], list[i].name);
+            strcpy(res[*count], list[i]->name);
             (*count)++;
-            remain -= list[i].price;
+            remain -= list[i]->price;
         }
     }
     return budget - remain;
 }
 
-int rank_cmp(const void *a, const void *b)
+int rank_cmp(const void *a, const void *b)    // 這裡type轉換要處理好
 {
-    struct item *x = (struct item*) a;
-    struct item *y = (struct item*) b;
-    return y->rank - x->rank;
-}
+    struct item **x = (struct item**) a;      // Origin: de-ref array address -> access array element 
+    struct item **y = (struct item**) b;      // Here: de-ref array address -> access the address of struct 
+    return (*y)->rank - (*x)->rank;           //       de-ref the address of struct -> access struct
+}                                             //       thus, we need to use double pointer
 
 int name_cmp(const void *a, const void *b)
 {
     char *x = (char*) a;
     char *y = (char*) b;
     return strcmp(x, y);
+}
+
+void clear_ptrs(struct item *list[], int n)
+{
+    for (int i = 0; i < n; i++)
+        free(list[i]);
 }
