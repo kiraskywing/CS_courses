86fcc2295264d5373eb38ae03a286d330449c9d8
Yi-Chang Lin <kiraskywing@gmail.com>
2021-01-27 15:45:17 +0800

    Add Udemy database materials 20210127

--- Introduction to Database Systems/Udemy/sql_commands.sql
+++ Introduction to Database Systems/Udemy/sql_commands.sql
@@ -18,8 +18,8 @@ SELECT database();
 -- Create & show table:
 CREATE TABLE tablename
   (
-    column_name data_type,
-    column_name data_type
+    column_name data_type NOT NULL DEFAULT ...,
+    column_name data_type NOT NULL DEFAULT ...,
     ...
   );
 
@@ -32,3 +32,17 @@ DROP TABLE tablename;
 
 -- Insert data:
 INSERT INTO table_name(column_name) VALUES (data);
+
+-- Show all datas in table:
+SELECT * FROM tablename;
+
+-- Show warnings:
+SHOW WARNINGS;
+
+-- Primary key & automatic increment:
+CREATE TABLE unique_cats2 (
+    cat_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100),
+    age INT,
+    PRIMARY KEY (cat_id)
+);
