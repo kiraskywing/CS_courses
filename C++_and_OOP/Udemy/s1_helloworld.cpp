7f2194785e9d2df5abcdcb46df9716f1accd5289
Yi-Chang Lin <kiraskywing@gmail.com>
2021-01-28 20:58:33 +0800

    Add Udemy C++_OOP material 20210128

--- C++_and_OOP/Udemy/s1_helloworld.cpp
+++ C++_and_OOP/Udemy/s1_helloworld.cpp
@@ -4,7 +4,7 @@ int main(void) {
     int num;
     std::cout << "Enter your favorite number between 1 to 100: ";
     std::cin >> num;
-    std::cout << "This is also my favorite number!" << std::endl;
+    std::cout << num << " is also my favorite number!" << std::endl;
     
     return 0;
 }
