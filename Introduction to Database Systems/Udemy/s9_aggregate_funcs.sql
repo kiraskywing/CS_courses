f0a2ac11c111c3a34182050b73e1a674a1a7c000
Yi-Chang Lin <kiraskywing@gmail.com>
2021-02-02 11:16:39 +0800

    Add Udemy database material 20210202

--- Introduction to Database Systems/Udemy/s9_aggregate_funcs.sql
+++ Introduction to Database Systems/Udemy/s9_aggregate_funcs.sql
@@ -5,3 +5,17 @@ SELECT COUNT(*) FROM books WHERE title LIKE "%the%";
 SELECT author_lname, COUNT(*) FROM books GROUP BY author_lname;
 SELECT author_fname, author_lname, COUNT(*) FROM books GROUP BY author_fname, author_lname;
 SELECT released_year, COUNT(*) FROM books GROUP BY released_year;
+
+SELECT MIN(released_year) FROM books;
+SELECT MAX(pages) FROM books;
+SELECT title, pages FROM books WHERE pages = (SELECT MIN(pages) FROM books);
+SELECT title, pages FROM books ORDER BY pages ASC LIMIT 1;
+
+SELECT author_fname, author_lname, MIN(released_year) FROM books GROUP BY author_fname, author_lname;
+SELECT CONCAT(author_fname, ' ', author_lname) AS author, MAX(pages) AS 'longest book' FROM books GROUP BY author_lname, author_fname;
+
+SELECT author_fname, author_lname, SUM(pages) FROM books GROUP BY author_fname, author_lname;
+
+SELECT released_year, AVG(stock_quantity) FROM books GROUP BY released_year;
+SELECT CONCAT(author_fname, " ", author_lname) AS full_name, AVG(pages) FROM books GROUP BY author_fname, author_lname;
+SELECT released_year AS "year", COUNT(*) AS "# books", AVG(pages) AS "avg pages" FROM books GROUP BY released_year ORDER BY released_year;
