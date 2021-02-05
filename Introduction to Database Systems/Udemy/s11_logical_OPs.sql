e74332d8836b7ab6cc0c40f6d330f2f290ec6367
Yi-Chang Lin <kiraskywing@gmail.com>
2021-02-05 10:59:36 +0800

    Add Udemy database materials 20210205

--- Introduction to Database Systems/Udemy/s11_logical_OPs.sql
+++ Introduction to Database Systems/Udemy/s11_logical_OPs.sql
@@ -14,7 +14,29 @@ SELECT title, released_year FROM books WHERE released_year <= 2000;
 SELECT title, author_lname, released_year FROM books WHERE author_lname='Eggers' AND released_year > 2010;
 SELECT title, author_lname, released_year, stock_quantity FROM books WHERE author_lname='Eggers' OR released_year > 2010 OR stock_quantity > 100;
 
-SELECT title, released_year FROM books WHERE released_year BETWEEN 2004 AND 2015; 
+SELECT title, released_year FROM books WHERE released_year BETWEEN 2004 AND 2015;    -- inclusive interval
 SELECT title, released_year FROM books WHERE released_year NOT BETWEEN 2004 AND 2015;
 SELECT name, birthdt FROM people WHERE birthdt BETWEEN '1980-01-01' AND '2000-01-01';
 SELECT name, birthdt FROM people WHERE birthdt BETWEEN CAST('1980-01-01' AS DATETIME) AND CAST('2000-01-01' AS DATETIME);  -- datatype conversion, recommend
+
+SELECT title, author_lname FROM books WHERE author_lname IN ('Carver', 'Lahiri', 'Smith');
+SELECT title, released_year FROM books WHERE released_year IN (2017, 1985);
+SELECT title, released_year FROM books WHERE released_year NOT IN (2000,2002,2004,2006,2008,2010,2012,2014,2016);
+SELECT title, released_year FROM books WHERE released_year >= 2000 AND released_year % 2 != 0 ORDER BY released_year;
+
+SELECT title, released_year, CASE WHEN released_year >= 2000 THEN 'Modern Lit' ELSE '20th Century Lit' END AS GENRE FROM books;
+SELECT title, stock_quantity,
+    CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        WHEN stock_quantity BETWEEN 101 AND 150 THEN '***'
+        ELSE '****'
+    END AS STOCK
+FROM books;
+SELECT title, stock_quantity,
+    CASE 
+        WHEN stock_quantity <= 50 THEN '*'
+        WHEN stock_quantity <= 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books; 
