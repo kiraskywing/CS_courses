55160a7a0282218c14a681512af11b9619a4150d
Yi-Chang Lin <kiraskywing@gmail.com>
2021-02-09 18:29:30 +0800

    Add Udemy database material 20210209

--- Introduction to Database Systems/Udemy/s12_oneToMany.sql
+++ Introduction to Database Systems/Udemy/s12_oneToMany.sql
@@ -46,3 +46,6 @@ SELECT first_name, last_name, order_date, amount FROM customers LEFT JOIN orders
 
 SELECT first_name, last_name, IFNULL(SUM(amount), 0) AS total_spent FROM customers
 LEFT JOIN orders ON customers.id = orders.customer_id GROUP BY customers.id ORDER BY total_spent;
+
+-- Right join
+SELECT * FROM customers RIGHT JOIN orders ON customers.id = orders.customer_id;
